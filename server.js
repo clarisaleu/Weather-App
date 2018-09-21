0b93a7aad6049dbd18443a29c9005545bfd3c536
Clarisa Leu-Rodriguez <clarisarodriguezaz@gmail.com>
2018-09-21 14:33:43 -0700

    Update port #

--- server.js
+++ server.js
@@ -1,36 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const app = express()
+const app = express();
 
 // TO DO FIX API KEY
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-  res.render('index', {weather: null, error: null});
-})
+app.get('/', function(req, res) {
+  res.render('index', { weather: null, error: null });
+});
 
-app.post('/', function (req, res) {
+app.post('/', function(req, res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${'3810563fa33964675dd1a10f3b6f7c15'}`
-  request(url, function (err, response, body) {
-    if(err){
-      res.render('index', {weather: null, error: 'Error, please try again'});
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${'3810563fa33964675dd1a10f3b6f7c15'}`;
+  request(url, function(err, response, body) {
+    if (err) {
+      res.render('index', { weather: null, error: 'Error, please try again' });
     } else {
-      let weather = JSON.parse(body)
-      if(weather.main == undefined){
-        res.render('index', {weather: null, error: 'Error, please try again'});
+      let weather = JSON.parse(body);
+      if (weather.main == undefined) {
+        res.render('index', {
+          weather: null,
+          error: 'Error, please try again'
+        });
       } else {
-        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
+        let weatherText = `It's ${weather.main.temp} degrees in ${
+          weather.name
+        }!`;
+        res.render('index', { weather: weatherText, error: null });
       }
     }
   });
-})
+});
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(8080, function() {
+  console.log('Example app listening on port 8080!');
+});
